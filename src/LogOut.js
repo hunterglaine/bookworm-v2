7422e3f2e67a3f7592ce645009300389039ff1c2
Hunter Laine <hunterglaine@gmail.com>
2021-03-25 20:30:22 -0700

    Made NavBar all in one line

---
+++ src/LogOut.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+function LogOut(props) {
+    
+    let history = useHistory();
+    
+    React.useEffect(() =>  {
+        fetch("/logout", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then (response => response.json())
+        .then(data => {
+                props.setUserLoggedIn({userId: null, userFirstName: null});
+                localStorage.setItem("userId", null)
+                localStorage.setItem("userFirstName", null)
+                history.push("/login")
+                alert(data["success"])
+            }
+    )}, []);
+
+        return <p>You have been successfully logged out!</p>
+}
+
+export default LogOut;
--- src/TopNavigation.js
+++ src/TopNavigation.js
@@ -11,45 +11,42 @@ import SearchBar from "./SearchBar"
 // Create and display the navigation bar at the top of the page - logo, search bar, create account button, and log in butto
 function TopNavigation(props) {
 
-    return (
+  return (
 
-        <Navbar variant="dark" className="color-nav d-flex navbar-expand-md" sticky="top" >
-          <Navbar.Brand className="p-2"> 
-          
-            <Link to={props.userLoggedIn.userId ? "/user/home/browsing" : "/login"}>
-              <img id="logo" src={bookworm_logo2} alt="Bookworm"/>
-            </Link>
-          </Navbar.Brand>
-            <SearchBar 
-              bookQuery={props.bookQuery} 
-              setBookQuery={props.setBookQuery} 
-              userCategories={props.userCategories} 
-              setUserCategories={props.setUserCategories} 
-            />     
-          <Nav as="ul" >
-            <Nav.Item as="li" style={{float: "right" }}>
-              {props.userLoggedIn.userId 
-                ? <Link className="link-button" to="/users-events">My Events</Link>
-                : null}
-            </Nav.Item>
-            <Nav.Item as="li" style={{float: "right" }}>
-              <Link className="link-button" to="/all-events">
-                All Bookworm Events
-              </Link>
-            </Nav.Item>
-            <Nav.Item as="li" style={{float: "right" }}>
-              <Link className="link-button" to={props.userLoggedIn.userId ? "/user/home/browsing" : "/create-account"}>
-                {props.userLoggedIn.userId ? "My Bookshelf" : "Create Account"}
-              </Link>
-            </Nav.Item>
-            <Nav.Item as="li" style={{float: "right" }}>
-              <Link className="link-button" to={props.userLoggedIn.userId ? "/logout" : "/login"}>
-                {props.userLoggedIn.userId ? "Log Out" : "Log In"}
-              </Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar>
-    );
+    <Navbar variant="dark" className="color-nav d-flex flex-row" sticky="top" >
+      <Navbar.Brand className="p-2"> 
+        <Link to={props.userLoggedIn.userId ? "/user/home/browsing" : "/login"}>
+          <img id="logo" src={bookworm_logo2} alt="Bookworm"/>
+        </Link>
+      </Navbar.Brand>
+        <SearchBar 
+          bookQuery={props.bookQuery} 
+          setBookQuery={props.setBookQuery} 
+          userCategories={props.userCategories} 
+          setUserCategories={props.setUserCategories} 
+        />     
+        <Nav.Item className="p-2" style={{float: "right" }}>
+          {props.userLoggedIn.userId 
+            ? <Link className="link-button" to="/users-events">My Events</Link>
+            : null}
+        </Nav.Item>
+        <Nav.Item className="p-2" style={{float: "right" }}>
+          <Link className="link-button" to="/all-events">
+            All Bookworm Events
+          </Link>
+        </Nav.Item>
+        <Nav.Item className="p-2" style={{float: "right" }}>
+          <Link className="link-button" to={props.userLoggedIn.userId ? "/user/home/browsing" : "/create-account"}>
+            {props.userLoggedIn.userId ? "My Bookshelf" : "Create Account"}
+          </Link>
+        </Nav.Item>
+        <Nav.Item className="p-2" style={{float: "right" }}>
+          <Link className="link-button" to={props.userLoggedIn.userId ? "/logout" : "/login"}>
+            {props.userLoggedIn.userId ? "Log Out" : "Log In"}
+          </Link>
+        </Nav.Item>
+    </Navbar>
+  );
 };
 
 export default TopNavigation;
